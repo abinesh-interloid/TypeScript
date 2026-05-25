fdbb057177d6801d9fa17dc0a8f2b486795c6e38
abinesh-interloid <abinesh@interloid.com>
2026-05-25 20:08:55 +0530

    completing of ts

---
+++ test/day_7.ts
@@ -0,0 +1,13 @@
+const a: Array<number> = [-8, -2000, -70, -10000];
+let great = -Infinity;
+let small = -Infinity;
+for (let i = 0; i < a.length; i++) {
+    if (a[i] > great) {
+        small = great;
+        great = a[i];
+    } else if (a[i] > small) {
+        small = a[i];
+    }
+}
+console.log(small);
+console.log(great);
